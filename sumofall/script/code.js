0b079c7d9c2965845cd57a4ad253cdfff750db1b
Thomas Zanis <56691946+campingtomz@users.noreply.github.com>
2020-07-14 15:44:26 -0400

    update
    Created the index.html. updated it. created the solve.html updated it with formating

---
+++ sumofall/script/code.js
@@ -0,0 +1,36 @@
+﻿
+$('#index-output').click(function () {
+    $('#output-index').show();
+    $('#output-code').hide();
+    $('#output-solve').hide();
+    $('#output-codejs').hide();
+    $('#output-solvejs').hide();
+});
+$('#code-output').click(function () {
+    $('#output-index').hide();
+    $('#output-code').show();
+    $('#output-solve').hide();
+    $('#output-codejs').hide();
+    $('#output-solvejs').hide();
+});
+$('#solve-output').click(function () {
+    $('#output-index').hide();
+    $('#output-code').hide();
+    $('#output-solve').show();
+    $('#output-codejs').hide();
+    $('#output-solvejs').hide();
+});
+$('#codejs-output').click(function () {
+    $('#output-index').hide();
+    $('#output-code').hide();
+    $('#output-solve').hide();
+    $('#output-codejs').show();
+    $('#output-solvejs').hide();
+});
+$('#solvejs-output').click(function () {
+    $('#output-index').hide();
+    $('#output-code').hide();
+    $('#output-solve').hide();
+    $('#output-codejs').hide();
+    $('#output-solvejs').show();
+});
---
+++ sumofall/script/solve.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    const $valueSpan = $('.valueSpan');
+    const $value = $('#arrayRange');
+    $valueSpan.html($value.val());
+    $value.on('input change', () => {
+
+        $valueSpan.html($value.val());
+    });
+});
